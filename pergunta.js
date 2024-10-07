a88ed52a6e5bec7f62caa5d39f296e644b8bd153
ZOPOLATTO <160533490+ZOPOLATTO@users.noreply.github.com>
2024-10-07 16:14:54 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Corinthians',
+    'Qual foi o ano de fundação?',
+    'Foi fundado em 1 de setembro de 1910'
+    )
+    
+    criaCartao(
+    'Geografia',
+    'Qual o maior país em termo de território',
+    'Russia'
+    )
+    
+    criaCartao(
+    'Futebol',
+    'Qual é o país com mais copa do mundo?',
+    'Seleção brasileira'
+    )
+    
+    criaCartao (
+    'Lingua Inglesa',
+    'Como se diz eu te amo em Ingles?',
+    'Oi em Ingles é i love you'
+    )
